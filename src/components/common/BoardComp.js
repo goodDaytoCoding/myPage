0f6ec7028213740905fdf490d77ba84775a9eac8
goodDayToCoding <jaeho614aa@gmail.com>
2024-09-04 23:41:06 +0900

    2024.09.04 board content css

--- src/components/common/BoardComp.js
+++ src/components/common/BoardComp.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import '../../lib/styles/Board.css';
+import '../../lib/styles/Portfolio.css';
+import '../../lib/styles/Stack.css';
+import '../../lib/styles/Profile.css';
 
 const BoardComp = ({ boardIndex, closeBoard }) => {
   if (boardIndex === null || boardIndex === false) return null;
@@ -10,17 +13,43 @@ const BoardComp = ({ boardIndex, closeBoard }) => {
   switch (boardIndex) {
     case 0:
       boardContent = (
-        <div>
-          <h1>This is the Profile Page</h1>
-          <div>안녕하세요 저는 최재호 입니다.</div>
-          <div>사진, 정보, 학교 등..</div>
+        <div className="profile">
+          <h1>Profile</h1>
+          <div className="profile-container">
+            <img src="public/textures/portfolio.jpg" alt="증명사진" />
+            <p>안녕하세요! 상상하는 것을 만들고 싶은 개발자 최재호 입니다.</p>
+            <div className="profile-age">
+              <h3>나이</h3>
+              <p>만 34세</p>
+            </div>
+            <div className="profile-educationlevel">
+              <h3>최종학력</h3>
+              <p>Kumoh National Institute of Technology</p>
+            </div>
+            <div className="profile-specialty">
+              <h3>전공</h3>
+              <p>전자 공학</p>
+            </div>
+            <div className="profile-job">
+              <h3>목표 직무</h3>
+              <p>Frontend Developer</p>
+            </div>
+            <div className="profile-hobbies">
+              <h3>취미</h3>
+              <p>독서, 운동, 노래부르기</p>
+            </div>
+            <div className="profile-contact">
+              <h3>Contact</h3>
+              <p>010-5393-0614</p>
+            </div>
+          </div>
         </div>
       );
       break;
     case 1:
       boardContent = (
         <div>
-          <h1>This is the About Me Page</h1>
+          <h1>About Me</h1>
           <div>자기소개</div>
         </div>
       );
@@ -34,7 +63,7 @@ const BoardComp = ({ boardIndex, closeBoard }) => {
             target="_blank" //새 탭에서 열림
             rel="noopener noreferrer" //새 탭 열릴 때 보안 문제 방지
           >
-            깃허브 리포지토리로 이동
+            Githib : Repository
           </a>
         </div>
       );
@@ -44,17 +73,53 @@ const BoardComp = ({ boardIndex, closeBoard }) => {
       break;
     case 4:
       boardContent = (
-        <div>
-          <h1>This is the Stack Page</h1>
-          <div>프로젝트 별 스택,img 포함</div>
+        <div className="stack">
+          <h1>Stack</h1>
+          <div className="stack-container">
+            <div className="stack-front">
+              <h3>FrontEnd</h3>
+              <img src="public/textures/stack.jpg" alt="stack_img" />
+            </div>
+            <div className="stack-back">
+              <h3>BackEnd</h3>
+              <img src="public/textures/stack.jpg" alt="stack_img" />
+            </div>
+            <div className="stack-version">
+              <h3>Version Control</h3>
+              <img src="public/textures/stack.jpg" alt="stack_img" />
+            </div>
+          </div>
         </div>
       );
       break;
     case 5:
       boardContent = (
-        <div>
-          <h1>This is the Portfolio Page</h1>
-          <div>포트폴리오 사이트 링크, 이미지, 설명</div>
+        <div className="portfolio">
+          <h1>Portfolio</h1>
+          <div className="portfolio-container">
+            <div className="portfolio-items">
+              <img src="public/textures/portfolio.jpg" alt="프로젝트 이름" />
+              <div className="portfolio-description">
+                <h2>이름: Tripper Maker</h2>
+                <time datetime="2023-06">2023년 6월 - 2023년 7월</time>
+                <span>참여 인원: 3명</span>
+                <p>
+                  이 프로젝트는 여행을 좋아하는 사람들이 정보와 후기를 공유하는
+                  사이트입니다.
+                </p>
+                <span>
+                  링크 :
+                  <a
+                    href="https://github.com/goodDaytoCoding"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Tripper Maker
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       );
       break;
